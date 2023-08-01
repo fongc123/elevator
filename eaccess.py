03624de93ded2ea06e00b6a13d03cae7dd132d13
Fong Chenananporn <>
2023-08-01 15:19:51 +0700

    added delay functionality

--- eaccess.py
+++ eaccess.py
@@ -11,6 +11,7 @@ import os
 
 RADD_FUNC_CODE = "17500000"
 QUERY_FUNC_CODE = "17200000"
+RDELAY_FUNC_CODE = "17800000"
 START_DATE = "20100101"
 END_DATE = "20291231"
 USER_PASS = "000000"
@@ -115,6 +116,15 @@ def radd1(ip_address : str, port : int, card_number : int, gates_allow : list, b
 
     return response
 
+def rdelay(ip_address : str, port : int, board_serial : int, door : int, control : int, delay : int):
+    hex_string = RDELAY_FUNC_CODE + ''.join(make_list(pad_zero(dec2hex(board_serial), 8))[::-1])
+    hex_string += f"0{door}" + f"0{control}" + f"0{delay}"
+    hex_string += "00"*53
+
+    response = sendPacket(ip_address, port, hex_string)
+
+    return response
+
 def rdel(ip_address : str, port : int, card_number : int, board_serial : int, start_date : int, end_date : int):
     hex_string = QUERY_FUNC_CODE + ''.join(make_list(pad_zero(dec2hex(board_serial), 8))[::-1]) + ''.join(make_list(pad_zero(dec2hex(card_number), 8))[::-1])
     hex_string += str(start_date) + str(end_date) + "00" + "000000" + USER_PASS + FIRST_CARD + MULTI_CARD + "0000" + "00" + RANDOM_BS
@@ -142,6 +152,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-radd", nargs=9, help="Add user rights")
     parser.add_argument("-radd1", nargs=7, help="Add user rights for door")
+    parser.add_argument("-rdelay", nargs=6, help="Delay door open")
     parser.add_argument("-rdel", nargs=7, help="Delete user rights")
     parser.add_argument("-getdata", nargs=4, help="Get recording data")
     args = parser.parse_args()
@@ -166,6 +177,14 @@ if __name__ == "__main__":
                 if int(value) > 1 or int(value) < 0:
                     raise Exception("Invalid gate allow value. Must be 0 or 1.")
             response = radd1(str(params[0]), int(params[1]), int(params[2]), str(params[3]).split(','), int(params[4]), int(params[5]), int(params[6]))
+        elif args.rdelay is not None:
+            # eaccess.exe -rdelay ip_adress port board_serial door control delay
+            params = args.rdelay
+            if int(params[3]) < 1 or int(params[3]) > 4:
+                raise Exception("Invalid door number. Must be between 1 and 4 inclusive.")
+            if int(params[4]) < 1 or int(params[4]) > 3:
+                raise Exception("Invalid control mode. Must be between 1 and 3 inclusive.")
+            response = rdelay(str(params[0]), int(params[1]), int(params[2]), int(params[3]), int(params[4]), int(params[5]))
         elif args.rdel is not None:
             # eaccess.exe -rdel ip_address port card_number board_serial start_date end_date path
             params = args.rdel
